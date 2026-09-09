1068b9d40c992bd90fa1b1a61dd2e7e0cb536cb3
Eden Zimbelman <eden.zimbelman@salesforce.com>
2026-09-08 21:16:48 -0700

    Revert "refactor(block-kit): align the data_table example with the node SDK"
    This reverts commit 91e8c167e127b78b53493257c44ffd8b6bce2ea2.

--- block-kit/src/main/java/blocks/DataTable.java
+++ block-kit/src/main/java/blocks/DataTable.java
@@ -3,6 +3,8 @@ package blocks;
 import com.slack.api.model.block.Blocks;
 import com.slack.api.model.block.DataTableBlock;
 import com.slack.api.model.block.composition.BlockCompositions;
+import com.slack.api.model.block.element.RichTextSectionElement;
+import com.slack.api.model.block.element.RichTextSectionElement.TextStyle;
 import java.util.List;
 
 /**
@@ -11,13 +13,53 @@ import java.util.List;
  */
 public class DataTable {
     /**
-     * A list of fruit and their quantities.
+     * A data table of departments with rich text badge cells.
      */
     public static DataTableBlock example01() {
-        DataTableBlock block = Blocks.dataTable(t -> t.caption("A list of fruit and their quantities")
+        DataTableBlock block = Blocks.dataTable(t -> t.caption("A Fabulous Table")
                 .rows(List.of(
-                        List.of(BlockCompositions.rawText("Fruit"), BlockCompositions.rawText("Quantity")),
-                        List.of(BlockCompositions.rawText("Apples"), BlockCompositions.rawNumber(12.0, "12")))));
+                        List.of(
+                                BlockCompositions.rawText("Name"),
+                                BlockCompositions.rawText("Department"),
+                                BlockCompositions.rawText("Badge")),
+                        List.of(
+                                BlockCompositions.rawText("Data Refinement Department"),
+                                BlockCompositions.rawText("MDR"),
+                                Blocks.richText(rt -> rt.elements(List.of(RichTextSectionElement.builder()
+                                        .elements(List.of(RichTextSectionElement.Text.builder()
+                                                .text("Blue")
+                                                .style(TextStyle.builder()
+                                                        .bold(true)
+                                                        .build())
+                                                .build()))
+                                        .build())))),
+                        List.of(
+                                BlockCompositions.rawText("Art Sourcing Department"),
+                                BlockCompositions.rawText("O&D"),
+                                Blocks.richText(rt -> rt.elements(List.of(RichTextSectionElement.builder()
+                                        .elements(List.of(
+                                                RichTextSectionElement.Text.builder()
+                                                        .text("Green")
+                                                        .build(),
+                                                RichTextSectionElement.Text.builder()
+                                                        .text("review")
+                                                        .style(
+                                                                TextStyle.builder()
+                                                                        .italic(true)
+                                                                        .build())
+                                                        .build()))
+                                        .build())))),
+                        List.of(
+                                BlockCompositions.rawText("Wellness Department"),
+                                BlockCompositions.rawText("Wellness Center"),
+                                Blocks.richText(rt -> rt.elements(List.of(RichTextSectionElement.builder()
+                                        .elements(List.of(RichTextSectionElement.Text.builder()
+                                                .text("Limited")
+                                                .style(TextStyle.builder()
+                                                        .bold(true)
+                                                        .build())
+                                                .build()))
+                                        .build())))))));
         return block;
     }
 }
--- block-kit/src/test/java/blocks/DataTableTest.java
+++ block-kit/src/test/java/blocks/DataTableTest.java
@@ -15,27 +15,126 @@ public class DataTableTest {
         String expected = """
             {
               "type": "data_table",
-              "caption": "A list of fruit and their quantities",
+              "caption": "A Fabulous Table",
               "rows": [
                 [
                   {
                     "type": "raw_text",
-                    "text": "Fruit"
+                    "text": "Name"
                   },
                   {
                     "type": "raw_text",
-                    "text": "Quantity"
+                    "text": "Department"
+                  },
+                  {
+                    "type": "raw_text",
+                    "text": "Badge"
                   }
                 ],
                 [
                   {
                     "type": "raw_text",
-                    "text": "Apples"
+                    "text": "Data Refinement Department"
                   },
                   {
-                    "type": "raw_number",
-                    "value": 12.0,
-                    "text": "12"
+                    "type": "raw_text",
+                    "text": "MDR"
+                  },
+                  {
+                    "type": "rich_text",
+                    "elements": [
+                      {
+                        "type": "rich_text_section",
+                        "elements": [
+                          {
+                            "type": "text",
+                            "text": "Blue",
+                            "style": {
+                              "bold": true,
+                              "italic": false,
+                              "strike": false,
+                              "highlight": false,
+                              "client_highlight": false,
+                              "underline": false,
+                              "unlink": false,
+                              "code": false
+                            }
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ],
+                [
+                  {
+                    "type": "raw_text",
+                    "text": "Art Sourcing Department"
+                  },
+                  {
+                    "type": "raw_text",
+                    "text": "O&D"
+                  },
+                  {
+                    "type": "rich_text",
+                    "elements": [
+                      {
+                        "type": "rich_text_section",
+                        "elements": [
+                          {
+                            "type": "text",
+                            "text": "Green"
+                          },
+                          {
+                            "type": "text",
+                            "text": "review",
+                            "style": {
+                              "bold": false,
+                              "italic": true,
+                              "strike": false,
+                              "highlight": false,
+                              "client_highlight": false,
+                              "underline": false,
+                              "unlink": false,
+                              "code": false
+                            }
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ],
+                [
+                  {
+                    "type": "raw_text",
+                    "text": "Wellness Department"
+                  },
+                  {
+                    "type": "raw_text",
+                    "text": "Wellness Center"
+                  },
+                  {
+                    "type": "rich_text",
+                    "elements": [
+                      {
+                        "type": "rich_text_section",
+                        "elements": [
+                          {
+                            "type": "text",
+                            "text": "Limited",
+                            "style": {
+                              "bold": true,
+                              "italic": false,
+                              "strike": false,
+                              "highlight": false,
+                              "client_highlight": false,
+                              "underline": false,
+                              "unlink": false,
+                              "code": false
+                            }
+                          }
+                        ]
+                      }
+                    ]
                   }
                 ]
               ]
